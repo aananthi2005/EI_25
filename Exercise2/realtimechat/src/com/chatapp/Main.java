2f0413c2b09c9f1fe0c7b58182510f25122368a3
aananthi2005 <aananthi@student.tce.edu>
2025-10-02 14:14:09 +0530

    Update Main.java

--- Exercise2/realtimechat/src/com/chatapp/Main.java
+++ Exercise2/realtimechat/src/com/chatapp/Main.java
@@ -1,9 +1,7 @@
-// File: src/com/chatapp/Main.java
+
 package com.chatapp;
 
-
 import com.chatapp.server.ChatServer;
-
 
 public class Main {
 public static void main(String[] args) {
@@ -16,12 +14,9 @@ System.out.println("Invalid port provided, using default 12345");
 }
 }
 
-
 ChatServer server = ChatServer.getInstance();
 server.start(port);
 
-
-// Add shutdown hook for graceful stop
 Runtime.getRuntime().addShutdownHook(new Thread(() -> {
 System.out.println("Shutting down server...");
 server.stop();
